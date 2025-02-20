e9fb78ab710186e57a2fe694ee50fa8b1451665c
Dibya <dibyarajghosh@Dibyas-MacBook-Air.local>
2025-02-20 12:34:31 +0530

    added cors

---
+++ src/main/java/com/civi/cms/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.civi.cms.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                List<String> origins = Arrays.asList(allowedOrigins.split(","));
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins.toArray(new String[0])) // Dynamic origins from properties
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow cookies
+            }
+        };
+    }
+}
+

f3f0b63c2fdf5faa38d99d5522d682fe92299410
Dibya <dibyarajghosh@Dibyas-MacBook-Air.local>
2025-04-22 21:00:31 +0530

    async email

--- src/main/java/com/civi/cms/CmsApplication.java
+++ src/main/java/com/civi/cms/CmsApplication.java
@@ -3,14 +3,15 @@ package com.civi.cms;
 import com.civi.cms.config.DatabaseUrlConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class
-CmsApplication {
+@EnableAsync
+public class CmsApplication {
 
 	public static void main(String[] args)
 	{
-		DatabaseUrlConverter.getJdbcUrl();
+		//DatabaseUrlConverter.getJdbcUrl();
 		SpringApplication.run(CmsApplication.class, args);
 
 

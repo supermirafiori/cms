aabd8cdc146f1861e9f1b99dfe4ebef62ab2acaa
Dibya <dibyarajghosh@Dibyas-MacBook-Air.local>
2025-05-02 09:38:16 +0530

    push

--- src/main/java/com/civi/cms/model/CaseDetails.java
+++ src/main/java/com/civi/cms/model/CaseDetails.java
@@ -14,8 +14,6 @@ import java.util.List;
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "caseId") // Prevents circular reference
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
-@Getter
-@Setter
 public class CaseDetails
 {
     @Id
@@ -39,6 +37,10 @@ public class CaseDetails
     private LocalDateTime dateOpened;
     private LocalDateTime dateClosed;
     private LocalDateTime nextReviewDate;
+
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "c", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<FollowUp> followUps;
@@ -95,5 +97,189 @@ public class CaseDetails
         IN_PROGRESS,
         HOLD,
         CLOSED
+    }
+
+    public Long getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Long caseId) {
+        this.caseId = caseId;
+    }
+
+    public String getCaseTitle() {
+        return caseTitle;
+    }
+
+    public void setCaseTitle(String caseTitle) {
+        this.caseTitle = caseTitle;
+    }
+
+    public String getCaseDescription() {
+        return caseDescription;
+    }
+
+    public void setCaseDescription(String caseDescription) {
+        this.caseDescription = caseDescription;
+    }
+
+    public Client getClientDetails() {
+        return clientDetails;
+    }
+
+    public void setClientDetails(Client clientDetails) {
+        this.clientDetails = clientDetails;
+    }
+
+    public CaseStatus getCaseStatus() {
+        return caseStatus;
+    }
+
+    public void setCaseStatus(CaseStatus caseStatus) {
+        this.caseStatus = caseStatus;
+    }
+
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public String getRiskLevel() {
+        return riskLevel;
+    }
+
+    public void setRiskLevel(String riskLevel) {
+        this.riskLevel = riskLevel;
+    }
+
+    public String getCaseCategory() {
+        return caseCategory;
+    }
+
+    public void setCaseCategory(String caseCategory) {
+        this.caseCategory = caseCategory;
+    }
+
+    public String getLegalInvolvement() {
+        return legalInvolvement;
+    }
+
+    public void setLegalInvolvement(String legalInvolvement) {
+        this.legalInvolvement = legalInvolvement;
+    }
+
+    public String getReferralSource() {
+        return referralSource;
+    }
+
+    public void setReferralSource(String referralSource) {
+        this.referralSource = referralSource;
+    }
+
+    public boolean isCaseWorkerAssigned() {
+        return isCaseWorkerAssigned;
+    }
+
+    public void setCaseWorkerAssigned(boolean caseWorkerAssigned) {
+        isCaseWorkerAssigned = caseWorkerAssigned;
+    }
+
+    public LocalDateTime getDateOpened() {
+        return dateOpened;
+    }
+
+    public void setDateOpened(LocalDateTime dateOpened) {
+        this.dateOpened = dateOpened;
+    }
+
+    public LocalDateTime getDateClosed() {
+        return dateClosed;
+    }
+
+    public void setDateClosed(LocalDateTime dateClosed) {
+        this.dateClosed = dateClosed;
+    }
+
+    public LocalDateTime getNextReviewDate() {
+        return nextReviewDate;
+    }
+
+    public void setNextReviewDate(LocalDateTime nextReviewDate) {
+        this.nextReviewDate = nextReviewDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<FollowUp> getFollowUps() {
+        return followUps;
+    }
+
+    public void setFollowUps(List<FollowUp> followUps) {
+        this.followUps = followUps;
+    }
+
+    public List<Member> getMembersInvolved() {
+        return membersInvolved;
+    }
+
+    public void setMembersInvolved(List<Member> membersInvolved) {
+        this.membersInvolved = membersInvolved;
+    }
+
+    public List<CaseAttachment> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<CaseAttachment> attachments) {
+        this.attachments = attachments;
+    }
+
+    public Long getAttachmentCount() {
+        return attachmentCount;
+    }
+
+    public void setAttachmentCount(Long attachmentCount) {
+        this.attachmentCount = attachmentCount;
+    }
+
+    public List<CaseHistory> getCaseHistories() {
+        return caseHistories;
+    }
+
+    public void setCaseHistories(List<CaseHistory> caseHistories) {
+        this.caseHistories = caseHistories;
+    }
+
+    public List<ServiceProvider> getServiceProviders() {
+        return serviceProviders;
+    }
+
+    public void setServiceProviders(List<ServiceProvider> serviceProviders) {
+        this.serviceProviders = serviceProviders;
     }
 }

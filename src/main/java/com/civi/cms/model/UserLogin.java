5c756e3c91c067fd299b031830a141b4579e0e96
Dibya <dibyarajghosh@Dibyas-MacBook-Air.local>
2025-05-02 09:47:44 +0530

    added new attribute

--- src/main/java/com/civi/cms/model/UserLogin.java
+++ src/main/java/com/civi/cms/model/UserLogin.java
@@ -2,10 +2,15 @@ package com.civi.cms.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
 public class UserLogin
 {
     @Id
@@ -15,55 +20,9 @@ public class UserLogin
     private boolean isLocked = false;
     private boolean isWorking=true;
 
+    @Transient
+    private String firstName;
     private LocalDateTime creationDate;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public boolean isLocked() {
-        return isLocked;
-    }
-
-    public void setLocked(boolean locked) {
-        isLocked = locked;
-    }
-
-    public boolean isWorking() {
-        return isWorking;
-    }
-
-    public void setWorking(boolean working) {
-        isWorking = working;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
 
     public enum Role{
         ADMIN,

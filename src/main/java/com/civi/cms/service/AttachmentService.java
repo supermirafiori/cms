fec7a1e351fe1e37f4ff4e8fa7f96f7228603149
Dibya <dibyarajghosh@Dibyas-MacBook-Air.local>
2025-05-12 22:23:09 +0530

    fix error

--- src/main/java/com/civi/cms/service/AttachmentService.java
+++ src/main/java/com/civi/cms/service/AttachmentService.java
@@ -2,12 +2,13 @@ package com.civi.cms.service;
 
 import com.civi.cms.model.Attachment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class AttachmentService {
     @Autowired
     private AttachmentRepository attachmentRepository;
